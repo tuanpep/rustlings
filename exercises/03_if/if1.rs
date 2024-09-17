18e359a01571dfc06c25f1b2b26a3fd934f59a56
Tuanpep <tuanbt209@gmail.com>
2024-09-17 14:43:58 +0700

    intro variables, functions, if

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,16 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    bigger(4, 5);
 }
 
 // Don't mind this for now :)
