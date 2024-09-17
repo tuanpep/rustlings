02f0e81cbc596ed792faa37cbfb71bb4b5ebd708
Tuanpep <tuanbt209@gmail.com>
2024-09-17 17:41:49 +0700

    primitive type and vec

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,9 +11,16 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apples: i32) -> i32 { 
+    if apples > 40 {    
+        apples
+    } else {
+        apples * 2
+    }
+ }
 
 fn main() {
-    // You can optionally experiment here.
+    calculate_price_of_apples(40);
 }
 
 // Don't change the tests!
